e0b23063461e0304b78f4de4a8317164bbe2cf0f
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-07-08 16:04:35 -0400

    fixed port for heroku

--- app/app.py
+++ app/app.py
@@ -41,4 +41,8 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        debug=False,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
